cf845c4fb31b196de7074b090465ac4769b889ed
Brandon Williams <brandon@bridgement.com>
2024-01-26 10:55:20 +0200

    F

--- component.ts
+++ component.ts
@@ -1,6 +1,6 @@
 import Component from "@glimmer/component";
-import { tracked } from "@glimmer/tracking";
-import { action, inject as service } from "@ember/service";
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
 
 export default class MyGraphComponent extends Component {
   @service lianaSession;
